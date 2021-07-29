f8cb1f2a7d8406a39e3f27a6b3b5552b9074d1d8
kosta080 <80321108+kosta080@users.noreply.github.com>
2021-07-29 08:41:51 +0300

    working on the drug and drop script

--- Assets/drag_on_screen.cs
+++ Assets/drag_on_screen.cs
@@ -23,8 +23,26 @@ public class drag_on_screen : MonoBehaviour
     void OnMouseUp()
     {
         dragging = false;
+        if (shootTestBeam())
+        {
+            Debug.Log("hit that");
+        }
     }
+    bool shootTestBeam()
+    {
+        var hitTargetplace = false;
+        RaycastHit[] hits;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
+        hits = Physics.RaycastAll(ray, 100.0F);
+
+        Debug.Log(hits);
+        Debug.Log(hits.Length);
+
+        
+
+        return hitTargetplace;
+    }
     void Update()
     {
         if (dragging)
@@ -37,6 +55,8 @@ public class drag_on_screen : MonoBehaviour
         if (stickToTarget)
         {
             transform.position = Vector3.Lerp(transform.position, targetplace.transform.position, Time.deltaTime * movespeed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetplace.transform.rotation, Time.deltaTime * movespeed);
+
             float dist = Vector3.Distance(transform.position, targetplace.transform.position);
             Debug.Log(dist);
             if (dist < 0.001f)
